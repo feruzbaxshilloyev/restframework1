dfceaa6a9b97dad8e1888caf76e5b8030d322358
feruzbaxshilloyev <baxshilloyevferuz23@gmail.com>
2025-03-26 00:37:11 +0500

    1

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateView, UpdateDestroyView
+from .views import CreateView, UpdateDestroyView, DeleteView, home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('create/', CreateView.as_view(), name='create'),
     path('update/<int:pk>/', UpdateDestroyView.as_view(), name='update'),
+    path('delete/<int:pk>/', DeleteView.as_view, name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,23 @@
+from django.shortcuts import render
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
 
 
-class CreateView(generics.ListCreateAPIView):
+def home(request):
+    return render(request, 'home.html')
+
+
+class CreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+class UpdateDestroyView(generics.UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class DeleteView(generics.DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
